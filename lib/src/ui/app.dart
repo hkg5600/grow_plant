e83175692845981f3bcf2c5ea5c982a137981d34
hkg5600 <46782752+hkg5600@users.noreply.github.com>
2020-07-06 17:29:44 +0900

    Test Camera

--- lib/src/ui/app.dart
+++ lib/src/ui/app.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
 class MyApp extends StatelessWidget {
   @override
@@ -7,17 +12,106 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.light(),
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: Text(
-            "TEST",
-            style: TextStyle(color: Colors.black),
-          ),
-        ),
         body: SafeArea(
-          child: Center(),
+          child: TakePictureScreen(),
         ),
       ),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class TakePictureScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => TakePictureScreenState();
+}
+
+class TakePictureScreenState extends State<TakePictureScreen> {
+  List<CameraDescription> cameras;
+  bool _isCameraAvailable = false;
+  CameraController _controller;
+  Future<void> _initializeControllerFuture;
+
+  Future<void> _setUpCamera() async {
+    try {
+      cameras = await availableCameras();
+      _controller = CameraController(
+        cameras.first,
+        ResolutionPreset.medium,
+      );
+      await _controller.initialize();
+      debugPrint("TEST");
+    } on CameraException catch (_) {
+      debugPrint(_.toString() + "THIS IS ERROR MESSAGE");
+    }
+
+    if (!mounted) return;
+    setState(() {
+      _isCameraAvailable = true;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _setUpCamera();
+  }
+
+  @override
+  void dispose() {
+    // 위젯의 생명주기 종료시 컨트롤러 역시 해제시켜줍니다.
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Take a picture')),
+      body: (_isCameraAvailable)
+          ? CameraPreview(_controller)
+          : Center(child: CircularProgressIndicator()),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera_alt),
+        // onPressed 콜백을 제공합니다.
+        onPressed: () async {
+          try {
+            await _initializeControllerFuture;
+            final path = join(
+              (await getTemporaryDirectory()).path,
+              '${DateTime.now()}.png',
+            );
+
+            await _controller.takePicture(path);
+
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DisplayPictureScreen(imagePath: path),
+              ),
+            );
+          } catch (e) {
+            print(e);
+          }
+        },
+      ),
+    );
+  }
+}
+
+// 사용자가 촬영한 사진을 보여주는 위젯
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Display the Picture')),
+      // 이미지는 디바이스에 파일로 저장됩니다. 이미지를 보여주기 위해 주어진
+      // 경로로 `Image.file`을 생성하세요.
+      body: Image.file(File(imagePath)),
     );
   }
 }
